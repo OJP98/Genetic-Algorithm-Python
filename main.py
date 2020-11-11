c66e78f567c84abb3775af6678e7c5794cd8d08f
OJP98 <ojuarez_p@hotmail.com>
2020-11-11 01:24:13 -0600

    Initial population and next generation code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import numpy as np
 from pprint import pprint
-from random import random, choices, uniform, randint, randrange
-from typing import List, Callable, Tuple
+from random import choices, randint, random, randrange, uniform
+from typing import Callable, List, Tuple
+
+import numpy as np
 
 Phenotype = Tuple[float, float, float]
 Population = List[Phenotype]
 MAX_VAL = 30
+MAX_GENERATIONS = 1
 
 
 def GeneratePhenotype(max_range: int) -> Phenotype:
@@ -16,10 +18,13 @@ def GeneratePopulation(size: int, max_genome_range: int) -> Population:
     return [GeneratePhenotype(max_genome_range) for _ in range(size)]
 
 
-def CrossOver(a: Phenotype, b: Phenotype) -> Phenotype:
-    rand = randint(0, 1)
-    fitness = a[2] if rand == 1 else b[2]
-    return [a[0], b[1], fitness]
+def CrossOver(a: Phenotype, b: Phenotype) -> Tuple[Phenotype, Phenotype]:
+    fitness1 = a[2] if randint(0, 1) == 1 else b[2]
+    fitness2 = a[2] if randint(0, 1) == 1 else b[2]
+
+    child1 = [a[0], b[1], fitness1]
+    child2 = [b[0], a[1], fitness2]
+    return [child1, child2]
 
 
 def Mutate(phenotype: Phenotype, num: int = 1, prob: float = 0.5) -> Phenotype:
@@ -38,13 +43,38 @@ def SortPopulation(population: Population) -> Population:
     return sorted(population, key=lambda x: x[2], reverse=True)
 
 
+def FilterPopulation(population: Population) -> Population:
+    return [phenotype for phenotype in population if phenotype[2] > 0]
+
+
 def Fitness(phenotype: Phenotype):
     pass
 
 
 if __name__ == '__main__':
 
-    population = GeneratePopulation(10, 10)
+    population = GeneratePopulation(size=4, max_genome_range=10)
     pprint(population)
-    print('='*20)
-    pprint(SortPopulation(population))
+
+    print('='*30)
+
+    for _ in range(MAX_GENERATIONS):
+        # Filter out those who have a fitness value of 0
+        filteredPopulation = FilterPopulation(population)
+
+        # Select the best phenotypes based on the fitness value
+        sortedPopulation = SortPopulation(population)
+        # TODO: Get x% of the sorted population
+
+        nextGen = []
+
+        for i in range(0, len(sortedPopulation), 2):
+            child1, child2 = CrossOver(
+                sortedPopulation[i], sortedPopulation[i + 1])
+            child1 = Mutate(child1)
+            child2 = Mutate(child2)
+            nextGen += [child1, child2]
+
+        population = nextGen
+
+        pprint(population)
